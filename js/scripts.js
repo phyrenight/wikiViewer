062888a85fa4f945dba9625a21527f10e4f3bdfe
Marc <phyrenight@gmail.com>
2016-09-14 14:24:10 -0400

    added timeout for wiki, and message displayed if nothing entered

--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,29 @@
 function wikiApi() {
-   /* var input = document.getElementById('searchItem');
-    var searchItem = input.value;*/
-    var searchItem = "cow"
-   console.log(searchItem)
+    var input = document.getElementById('searchItem');
+    var searchItem = input.value;
     var $links = $('#links');
-	var wikiUrl = "https://en.wikipedia.org/w/api.php?action=opensearch&search=" +
-	searchItem + "&format=json&callback=wikiCAllBack";
-	$.ajax({
-	  url: wikiUrl,
-	  dataType: 'jsonp',
-	  success: function(data) {
-	    var searchList = data[1];
-	    for(var i = 0; i < searchList.length; i++){
-	      var item = searchList[i];
-	      var url = "http://en.wikipedia.org/wiki/" + item;
-	      $links.append("<li><a href='" + url + "'>" + item + "</a></li><hr>");
+    var TimeOutRequest = setTimeout(function(){
+    	$links.text("Wiki failed to load");
+    },8000);
+    $links.empty()
+    if(searchItem){
+	  var wikiUrl = "https://en.wikipedia.org/w/api.php?action=opensearch&search=" +
+	  searchItem + "&format=json&callback=wikiCAllBack";
+	  $.ajax({
+	    url: wikiUrl,
+	    dataType: 'jsonp',
+	    success: function(data) {
+	      var searchList = data[1];
+	      for(var i = 0; i < searchList.length; i++){
+	        var item = searchList[i];
+	        var url = "http://en.wikipedia.org/wiki/" + item;
+	        $links.append("<li><a href='" + url + "'>" + item + "</a></li>");
+	      }
+	      clearTimeout(TimeOutRequest)
 	    }
-	  }
-	})
+	  })
+	}
+	else{
+		$links.append("Nothing entered!");
+	}
 }
