7a6f395316882dc2334b2ec4fe7f783c90f1b1a0
Marc <phyrenight@gmail.com>
2016-10-07 03:54:16 -0400

    validator ran on index.html and scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -2,36 +2,37 @@
 
 // find a way to implement DRY, the search and random should pass parameter(url) to a function containing the $.ajax call
 function wikiSearch() {
-	var input = document.getElementById('searchItem');
-    var searchItem = input.value;
-    var $links = $('#links');
-    $links.empty()
-    if(searchItem){
-	  var wikiUrl = "https://en.wikipedia.org/w/api.php?action=opensearch&search=" +
-	  searchItem + "&format=json&callback=wikiCAllBack";
-	  wikiApi(wikiUrl);
-    }else{
-    	$links.append("Nothing entered")
-    }
+  var input = document.getElementById('searchItem');
+  var searchItem = input.value;
+  var $links = $('#links');
+  $links.empty();
+  if(searchItem){
+	var wikiUrl = "https://en.wikipedia.org/w/api.php?action=opensearch&search=" +
+	searchItem + "&format=json&callback=wikiCAllBack";
+	wikiApi(wikiUrl);
+  }
+  else{
+    $links.append("Nothing entered");
+  }
 }
 function wikiApi(wikiUrl){
-	var $links = $('#links');
-	var TimeOutRequest = setTimeout(function(){
-          $links.text("Wiki failed to load");
-        },8000);
-	$links.empty();
-	  $.ajax({
-	    url: wikiUrl,
-	    dataType: 'jsonp',
-	    success: function(data) {
-	      var searchList = data[1];
-	     for(var i = 0; i < searchList.length; i++){
-	        var item = searchList[i];
-	        var url = "http://en.wikipedia.org/wiki/" + item;
-	        $links.append("<li><a href='" + url + "'>" + item + "</a></li>");
-	      }
-	     clearTimeout(TimeOutRequest)
-	    }
-	  });
+  var $links = $('#links');
+  var TimeOutRequest = setTimeout(function(){
+    $links.text("Wiki failed to load");
+  },8000);
+  $links.empty();
+  $.ajax({
+    url: wikiUrl,
+    dataType: 'jsonp',
+    success: function(data) {
+      var searchList = data[1];
+      for(var i = 0; i < searchList.length; i++){
+        var item = searchList[i];
+        var url = "http://en.wikipedia.org/wiki/" + item;
+        $links.append("<li><a href='" + url + "'>" + item + "</a></li>");
+      }
+      clearTimeout(TimeOutRequest);
+    }
+  });
 }
 
